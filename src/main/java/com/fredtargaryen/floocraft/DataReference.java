ceb6e6f660e5a0b0ac3726c6d575b44fdedbaf4f
fredtargaryen <fredtargaryen@gmail.com>
2014-09-26 19:16:04 +0100

    Officially released!

--- src/main/java/com/fredtargaryen/floocraft/DataReference.java
+++ src/main/java/com/fredtargaryen/floocraft/DataReference.java
@@ -10,24 +10,6 @@ package com.fredtargaryen.floocraft;
  * MessageTeleportEntity: teleports player that sent this to (destX, destY, destZ) if possible
  * MessageTileEntityFireplaceFunction: lets the server know whether the sign at those coordinates is decorative or practical
  *
- * ===FLOOQUIVALENT CLASSES===
- * TileEntitySign --> TileEntityFireplace
- * TileEntityFireplace is created by BlockFlooSign
- * TileEntityFire is created by GreenFlamesBusyLower
- * TileEntitySignRenderer --> TileEntityFlooSignRenderer
- * GuiEditSign --> GuiFlooSign
- *
- * ===GUI(DE)===
- * ItemFlooSign loads GuiFlooSign
- * GreenFlamesBusyLower loads GuiTeleport
- *
- * ===Player Hierarchy===
- * EntityPlayer
- * --AbstractClientPlayer
- * ----EntityPlayerSP
- * ------EntityClientPlayerMP
- * --EntityPlayerMP
- *
  * When changing version number, change in: DataReference, build.gradle, mcmod.info
  */
 
@@ -36,7 +18,7 @@ public class DataReference
     //MAIN MOD DETAILS
     public static final String MODID = "ftfloocraft";
     public static final String MODNAME = "Floocraft";
-    public static final String VERSION = "1.7.10-0.3.1";
+    public static final String VERSION = "1.7.10-1.0";
     //PROXY PATHS
     public static final String CLIENTPROXYPATH = "com.fredtargaryen.floocraft.proxy.ClientProxy";
     public static final String SERVERPROXYPATH = "com.fredtargaryen.floocraft.proxy.ServerProxy";
