c70db2a97cff381e9c7a86018795782856d94e30
Unknown <lggy@hotmail.co.uk>
2019-06-18 15:51:54 +0100

    Credit XuyuEre for localisation

--- src/main/java/com/fredtargaryen/floocraft/DataReference.java
+++ src/main/java/com/fredtargaryen/floocraft/DataReference.java
@@ -26,7 +26,7 @@ public class DataReference {
     //MAIN MOD DETAILS
     public static final String MODID = "floocraftft";
     public static final String MODNAME = "Floocraft";
-    public static final String VERSION = "1.9.5";
+    public static final String VERSION = "1.9.6";
     //PROXY PATHS
     public static final String CLIENTPROXYPATH = "com.fredtargaryen.floocraft.proxy.ClientProxy";
     public static final String SERVERPROXYPATH = "com.fredtargaryen.floocraft.proxy.ServerProxy";
