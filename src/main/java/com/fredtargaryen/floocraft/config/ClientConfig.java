15a4278b7666a19471c03ca7787faafedb720e57
Unknown <lggy@hotmail.co.uk>
2021-08-21 15:53:23 +0100

    Add translation keys for config variable descriptions
    Only en_us, en_gb and en_pt are correct right now.
    
    Opens #43

--- src/main/java/com/fredtargaryen/floocraft/config/ClientConfig.java
+++ src/main/java/com/fredtargaryen/floocraft/config/ClientConfig.java
@@ -9,8 +9,10 @@ public class ClientConfig {
 
     public static void init(ForgeConfigSpec.Builder clientBuilder) {
         ENABLE_DIZZY = clientBuilder.comment("If true, there is a disorienting camera-wobbling effect when you teleport with Floo Powder.")
+                .translation("config.client.dizzy")
                 .define("effect.dizzy", true);
         ENABLE_FLASH = clientBuilder.comment("If true, there is a green or magenta flash when you teleport with Floo Powder.")
+                .translation("config.client.flash")
                 .define("effect.flash", true);
     }
 }
--- src/main/java/com/fredtargaryen/floocraft/config/CommonConfig.java
+++ src/main/java/com/fredtargaryen/floocraft/config/CommonConfig.java
@@ -15,14 +15,19 @@ public class CommonConfig {
 
     public static void init(ForgeConfigSpec.Builder commonBuilder) {
         DEPLETE_FLOO = commonBuilder.comment("If true, fires from craftable Floo Powder will allow a limited number of teleports before reverting to normal fire.")
+                .translation("config.common.deplete")
                 .define("tp.deplete", true);
         VILLAGERS_TELEPORT = commonBuilder.comment("If true, villagers who wander into Floo fires MAY teleport to a random fireplace. Never consumes Floo Powder.")
+                .translation("config.common.villagertp")
                 .define("tp.villagers", false);
         ITEMS_TELEPORT = commonBuilder.comment("If true, dropped items that touch Floo fires WILL teleport to a random fireplace. Never consumes Floo Powder.")
+                .translation("config.common.itemtp")
                 .define("tp.items", false);
         MISC_MOBS_TELEPORT = commonBuilder.comment("As with villagers, but for Sheep, Cows, Spiders, Silverfish, Zombies etc. Never consumes Floo Powder.")
+                .translation("config.common.mobtp")
                 .define("tp.miscmobs", false);
         FLOO_SAFETY_TIME = commonBuilder.comment("When arriving in a normal fire, the fire converts to Floo fire temporarily. It will stay like that for this many ticks, before returning to normal.")
+                .translation("config.common.safety")
                 .defineInRange("tp.safety", 50, 1, 100);
     }
 }
