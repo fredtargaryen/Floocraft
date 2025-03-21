47afc82bf4c9cb0bb6b5a26984ff0eed63c41c6a
FredTargaryen <lggy@hotmail.co.uk>
2025-03-21 17:29:38 +0000

    Fix some issues; some Floo Sign name validation; Floo Flames extinguish change (#56)
    * Change "you are here" to just "here" and add appropriate translations
    
    * Use single array-to-location-name converter which trims leading/trailing whitespace and limits location names to 160 characters
    
    * Block requested location names if they end up empty after conversion
    
    * Floo campfires drop like their vanilla counterparts. Closes #54
    
    * Fix Floo fires still burning when the block under them is destroyed
    
    * Give Floo fire the same bounding box as vanilla fire
    
    * Floo flames extinguish like vanilla fire. Closes #52

--- src/main/java/com/fredtargaryen/floocraft/block/FlooFlamesBlock.java
+++ src/main/java/com/fredtargaryen/floocraft/block/FlooFlamesBlock.java
@@ -1,6 +1,7 @@
 package com.fredtargaryen.floocraft.block;
 
 import com.fredtargaryen.floocraft.DataReference;
+import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.particles.ParticleTypes;
@@ -8,11 +9,15 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoulFireBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
@@ -25,9 +30,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FlooFlamesBlock extends FlooMainTeleporterBase {
-    private static final Direction[] HORIZONTALS = new Direction[] { Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST };
-    private static final VoxelShape SMALLBOX = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
-    private static final VoxelShape TALLBOX = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 32.0D, 16.0D);
+    private static final Direction[] HORIZONTALS = new Direction[]{Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST};
+    private static final VoxelShape BOX = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D);
 
     /**
      * The fire "behaviour". 0 for temporary; 1 for idle; 2 for busy
@@ -49,13 +53,14 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
                     if (state.getValue(COLOUR) == STANDARD) {
                         if (state.getValue(BEHAVIOUR) == IDLE) return 12;
                         return 15;
-                    }
-                    else {
+                    } else {
                         if (state.getValue(BEHAVIOUR) == IDLE) return 7;
                         return 10;
                     }
                 })
-                .replaceable());
+                .replaceable()
+                .sound(SoundType.EMPTY)
+                .noTerrainParticles());
     }
 
     @Override
@@ -66,11 +71,7 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
 
     @Override
     protected @Nonnull VoxelShape getShape(BlockState state, @Nonnull BlockGetter blockGetter, @Nonnull BlockPos pos, @Nonnull CollisionContext context) {
-        if (state.getValue(BEHAVIOUR) == IDLE)
-        {
-            return SMALLBOX;
-        }
-        return TALLBOX;
+        return BOX;
     }
 
     @Override
@@ -84,10 +85,19 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
     }
 
     @Override
+    protected BlockState updateShape(
+            BlockState myState, Direction direction, BlockState neighbouringState, LevelAccessor accessor, BlockPos pos, BlockPos neighbourPos
+    ) {
+        return direction == Direction.DOWN && !this.canSurvive(myState, accessor, pos)
+                ? Blocks.AIR.defaultBlockState()
+                : super.updateShape(myState, direction, myState, accessor, pos, neighbourPos);
+    }
+
+    @Override
     protected void tick(@Nonnull BlockState state, @Nonnull ServerLevel level, @Nonnull BlockPos pos, @Nonnull RandomSource rand) {
         switch (state.getValue(BEHAVIOUR)) {
             case BUSY:
-                if(level.getNearestPlayer((double)pos.getX() + 0.5D, (double)pos.getY() + 0.5D, (double)pos.getZ() + 0.5D, DataReference.FLOO_FIRE_DETECTION_RANGE, false) == null) {
+                if (level.getNearestPlayer((double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, DataReference.FLOO_FIRE_DETECTION_RANGE, false) == null) {
                     boolean colour = SoulFireBlock.canSurviveOnBlock(level.getBlockState(pos.below())) ? SOUL : STANDARD;
                     level.setBlockAndUpdate(pos, state
                             .setValue(COLOUR, colour)
@@ -96,8 +106,7 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
                 super.tick(state, level, pos, rand);
                 break;
             case IDLE:
-                if(level.getNearestPlayer((double)pos.getX() + 0.5D, (double)pos.getY() + 0.5D, (double)pos.getZ() + 0.5D, DataReference.FLOO_FIRE_DETECTION_RANGE, false) != null)
-                {
+                if (level.getNearestPlayer((double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, DataReference.FLOO_FIRE_DETECTION_RANGE, false) != null) {
                     boolean colour = SoulFireBlock.canSurviveOnBlock(level.getBlockState(pos.below())) ? SOUL : STANDARD;
                     level.setBlockAndUpdate(pos, state
                             .setValue(COLOUR, colour)
@@ -121,6 +130,23 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
     }
 
     @Override
+    public boolean canDepartFromBlock(BlockState state) {
+        return state.getValue(BEHAVIOUR) != TEMP;
+    }
+
+    @Override
+    protected boolean canSurvive(BlockState pState, LevelReader pLevel, BlockPos pPos) {
+        BlockPos blockpos = pPos.below();
+        return pLevel.getBlockState(blockpos).isFaceSturdy(pLevel, blockpos, Direction.UP);
+    }
+
+    // Client effects
+
+    @Override
+    protected void spawnDestroyParticles(Level pLevel, Player pPlayer, BlockPos pPos, BlockState pState) {
+    }
+
+    @Override
     public void animateTick(@Nonnull BlockState state, @Nonnull Level level, @Nonnull BlockPos pos, RandomSource rand) {
         if (rand.nextInt(24) == 0) {
             level.playLocalSound(pos, SoundEvents.FIRE_AMBIENT, SoundSource.BLOCKS, 1.0F + rand.nextFloat(), rand.nextFloat() * 0.7F + 0.3F, false);
@@ -129,16 +155,25 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
     }
 
     protected void doSmokeParticles(@Nonnull BlockState state, @Nonnull Level level, @Nonnull BlockPos pos, RandomSource rand) {
-        if(rand.nextInt(8) == 0) {
-            double d0 = (double)pos.getX() + rand.nextDouble();
-            double d1 = (double)pos.getY() + rand.nextDouble() * 0.5D + 0.5D;
-            double d2 = (double)pos.getZ() + rand.nextDouble();
+        if (rand.nextInt(8) == 0) {
+            double d0 = (double) pos.getX() + rand.nextDouble();
+            double d1 = (double) pos.getY() + rand.nextDouble() * 0.5D + 0.5D;
+            double d2 = (double) pos.getZ() + rand.nextDouble();
             level.addParticle(ParticleTypes.LARGE_SMOKE, d0, d1, d2, 0.0D, 0.0D, 0.0D);
+        }
+    }
+
+    @Override
+    public void destroy(LevelAccessor level, BlockPos pos, BlockState state) {
+        super.destroy(level, pos, state);
+        if (level.isClientSide()) {
+            Minecraft.getInstance().level.playLocalSound(pos, SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS, 1f, 1f, true);
         }
     }
 
     /**
      * When placed, when should the next tick start?
+     *
      * @return the number of ticks to wait before the first tick
      */
     protected int getTimeToFirstTick() {
@@ -150,8 +185,9 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
     /**
      * Find the first solid block above this one.
      * N.B. the method assumes this is being called from a Floo fire block's position
+     *
      * @param level the level this fire block is in
-     * @param pos the position of this fire block
+     * @param pos   the position of this fire block
      * @return the Y position of the first solid block above this one.
      * Return Integer.MAX_VALUE if the max build height is reached without
      * finding a solid block
@@ -172,9 +208,10 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
 
     /**
      * Check for an uninterrupted column of solid blocks.
-     * @param level the level this block is in
+     *
+     * @param level     the level this block is in
      * @param bottomPos the position of the bottom of the column
-     * @param topY the Y position of the block above where the fire would be
+     * @param topY      the Y position of the block above where the fire would be
      * @return if there was an unbroken column from bottomPos.getY() to topY
      */
     private boolean isWallColumn(Level level, BlockPos bottomPos, int topY) {
@@ -183,7 +220,7 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
         BlockState bs;
         while (y < topY) {
             bs = level.getBlockState(nextPos);
-            if (bs.canBeReplaced())  return false;
+            if (bs.canBeReplaced()) return false;
             nextPos = nextPos.above();
             y++;
         }
@@ -259,8 +296,9 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
      * Returns the Direction that points out of the fireplace, if the fireplace is valid.
      * Not necessarily the direction of the sign.
      * If the fireplace is invalid, returns null.
+     *
      * @param level The level the flames block is in
-     * @param pos The position of the flames block
+     * @param pos   The position of the flames block
      * @return Direction.UP if the fire block is in a corner of a valid fireplace; NORTH, SOUTH, EAST, WEST if the fire
      * is in a valid fireplace but not a corner; null if the fireplace is invalid
      */
@@ -278,8 +316,8 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
         switch (walls.size()) {
             case 3:
                 //One-block-long fireplace
-                for(Direction ef : HORIZONTALS) {
-                    if(!walls.contains(ef)) return ef;
+                for (Direction ef : HORIZONTALS) {
+                    if (!walls.contains(ef)) return ef;
                 }
                 break;
             case 2:
@@ -287,17 +325,15 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
                         || (walls.contains(Direction.SOUTH) && (walls.contains(Direction.WEST) || walls.contains(Direction.EAST))))) {
                     boolean zeroToOne = this.canLoopToCorner(level, x, y, z, walls.get(0), walls.get(1), t);
                     boolean oneToZero = this.canLoopToCorner(level, x, y, z, walls.get(1), walls.get(0), t);
-                    if(zeroToOne && oneToZero) {
+                    if (zeroToOne && oneToZero) {
                         //Fire is in corner of fireplace. Valid fireplace, but can't put a sign on a corner, so
                         //return UP
                         return Direction.UP;
-                    }
-                    else if(zeroToOne) {
+                    } else if (zeroToOne) {
                         //End of a long fireplace
                         //Wall 0 is the back of the fireplace
                         return walls.get(0).getOpposite();
-                    }
-                    else if(oneToZero) {
+                    } else if (oneToZero) {
                         //End of a long fireplace
                         //Wall 1 is the back of the fireplace
                         return walls.get(1).getOpposite();
@@ -308,20 +344,20 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
                 switch (walls.get(0)) {
                     //This will be the back wall. If valid, this is the middle of a long fireplace
                     case NORTH:
-                        if      (this.canLoopToCorner(level, x, y, z, Direction.NORTH, Direction.WEST, t)
-                                &&  this.canLoopToCorner(level, x, y, z, Direction.NORTH, Direction.EAST, t))
+                        if (this.canLoopToCorner(level, x, y, z, Direction.NORTH, Direction.WEST, t)
+                                && this.canLoopToCorner(level, x, y, z, Direction.NORTH, Direction.EAST, t))
                             return Direction.SOUTH;
                     case WEST:
-                        if      (this.canLoopToCorner(level, x, y, z, Direction.WEST, Direction.SOUTH, t)
-                                &&  this.canLoopToCorner(level, x, y, z, Direction.WEST, Direction.NORTH, t))
+                        if (this.canLoopToCorner(level, x, y, z, Direction.WEST, Direction.SOUTH, t)
+                                && this.canLoopToCorner(level, x, y, z, Direction.WEST, Direction.NORTH, t))
                             return Direction.EAST;
                     case EAST:
-                        if      (this.canLoopToCorner(level, x, y, z, Direction.EAST, Direction.SOUTH, t)
-                                &&  this.canLoopToCorner(level, x, y, z, Direction.EAST, Direction.NORTH, t))
+                        if (this.canLoopToCorner(level, x, y, z, Direction.EAST, Direction.SOUTH, t)
+                                && this.canLoopToCorner(level, x, y, z, Direction.EAST, Direction.NORTH, t))
                             return Direction.WEST;
                     case SOUTH:
-                        if      (this.canLoopToCorner(level, x, y, z, Direction.SOUTH, Direction.WEST, t)
-                                &&  this.canLoopToCorner(level, x, y, z, Direction.SOUTH, Direction.EAST, t))
+                        if (this.canLoopToCorner(level, x, y, z, Direction.SOUTH, Direction.WEST, t)
+                                && this.canLoopToCorner(level, x, y, z, Direction.SOUTH, Direction.EAST, t))
                             return Direction.NORTH;
                         break;
                 }
@@ -330,10 +366,5 @@ public class FlooFlamesBlock extends FlooMainTeleporterBase {
                 break;
         }
         return null;
-    }
-
-    @Override
-    public boolean canDepartFromBlock(BlockState state) {
-        return state.getValue(BEHAVIOUR) != TEMP;
     }
 }
