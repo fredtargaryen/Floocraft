cfdcad0432cb05b3a0db499f8ad5fa700cc520ec
fredtargaryen <lggy@hotmail.co.uk>
2025-03-21 17:18:40 +0000

    Use single array-to-location-name converter which trims leading/trailing whitespace and limits location names to 160 characters

--- src/main/java/com/fredtargaryen/floocraft/HelperFunctions.java
+++ src/main/java/com/fredtargaryen/floocraft/HelperFunctions.java
@@ -3,20 +3,9 @@ package com.fredtargaryen.floocraft;
 import java.util.List;
 
 public class HelperFunctions {
-    public static String convertArrayToLocationName(String[] array) {
-        return String.format("%s %s %s %s",
-                        array[0],
-                        array[1],
-                        array[2],
-                        array[3]);
-    }
-
     public static String convertArrayToLocationName(List<String> array) {
-        return String.format("%s %s %s %s",
-                array.get(0),
-                array.get(1),
-                array.get(2),
-                array.get(3));
+        String joined = String.join(" ", array);
+        return joined.substring(0, Math.min(joined.length(), 160)).trim();
     }
 
     public static float getElapsedPartialTicks(float oldPt, float newPt) {
